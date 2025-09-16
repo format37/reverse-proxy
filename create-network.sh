803d0841fedd91a9e965e8245346150b533469cd
Alex <format37@gmail.com>
2025-09-16 12:48:24 +0400

    network creation with sudo

--- create-network.sh
+++ create-network.sh
@@ -7,6 +7,6 @@ if docker network inspect "$NETWORK_NAME" >/dev/null 2>&1; then
   echo "Docker network '$NETWORK_NAME' already exists."
 else
   echo "Creating docker network '$NETWORK_NAME'..."
-  docker network create "$NETWORK_NAME"
+  sudo docker network create "$NETWORK_NAME"
   echo "Docker network '$NETWORK_NAME' created."
 fi
